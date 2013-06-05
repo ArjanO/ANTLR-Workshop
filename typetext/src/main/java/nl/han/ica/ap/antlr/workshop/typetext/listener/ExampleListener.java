ae5b54fc8fa28ee13dc633d0f9f5a424b92f0bb1
ArjanO <ArjanO@outlook.com>
2013-06-05 11:51:33 +0200

    Added ExampleListener.

---
+++ typetext/src/main/java/nl/han/ica/ap/antlr/workshop/typetext/listener/ExampleListener.java
@@ -0,0 +1,26 @@
+package nl.han.ica.ap.antlr.workshop.typetext.listener;
+
+import nl.han.ica.ap.antlr.workshop.TypeBaseListener;
+import nl.han.ica.ap.antlr.workshop.TypeParser.TextContext;
+import nl.han.ica.ap.antlr.workshop.TypeParser.TypeContext;
+
+/**
+ * Example implementation with listener.
+ * 
+ * @author Arjan and Tim
+ */
+public class ExampleListener extends TypeBaseListener {
+	private boolean printText;
+	
+	@Override
+	public void enterType(TypeContext ctx) {
+		printText = ctx.getText().equals("Boek");
+	}
+	
+	@Override
+	public void enterText(TextContext ctx) {
+		if (printText) {
+			System.out.println(ctx.getText());
+		}
+	}
+}
