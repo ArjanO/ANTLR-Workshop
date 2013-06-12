e01d92ad674ae4d9b4fea7036500d37c4eb98f6f
ArjanO <ArjanO@outlook.com>
2013-06-12 08:56:43 +0200

    Expect that getClassByName is called at least once.

--- nlp/src/test/java/nl/han/ica/ap/antlr/workshop/nlp/listener/ZelfstandignaamwoordListenerTest.java
+++ nlp/src/test/java/nl/han/ica/ap/antlr/workshop/nlp/listener/ZelfstandignaamwoordListenerTest.java
@@ -104,12 +104,12 @@ public class ZelfstandignaamwoordListenerTest {
 		Association class1Association = new Association(class2);
 		
 		EasyMock.expect(class1.addAssociation(class2))
-			.andReturn(class1Association).anyTimes();
+			.andReturn(class1Association).atLeastOnce();
 		
 		EasyMock.expect(classController.getClassByName(class1Name))
-			.andReturn(class1).anyTimes();
+			.andReturn(class1).atLeastOnce();
 		EasyMock.expect(classController.getClassByName(class2Name))
-			.andReturn(class2).anyTimes();
+			.andReturn(class2).atLeastOnce();
 		
 		EasyMock.replay(class1);
 		EasyMock.replay(class2);
